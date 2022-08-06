312d9d4b26f624209fe06d2d0f3e35f73409ac35
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-08-06 21:22:54 +0300

    Create codebreaker.py

---
+++ codebreaker.py
@@ -0,0 +1,21 @@
+# Programa rápido para descoberta de padrão
+
+from json import encoder
+import math
+# from imp import SEARCH_ERROR
+import os
+from os import listdir
+from os.path import isfile, join
+import re
+
+import textwrap
+
+# List of characters in hex
+# buddy = 0x42 0x55 0x44 0x44 0x59
+term_character_list = [0x42, 0x55, 0x44, 0x44, 0x59]
+
+
+def find_smallest_number()
+{
+    print('aaa')
+}
