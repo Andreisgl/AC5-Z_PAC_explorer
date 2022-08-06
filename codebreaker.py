f6bb85975932716f1c9240a20bcfe46ba9ce9f37
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-08-06 23:28:32 +0300

    develop codebreaker

--- codebreaker.py
+++ codebreaker.py
@@ -1,21 +1,58 @@
 # Programa rápido para descoberta de padrão
 
-from json import encoder
-import math
-# from imp import SEARCH_ERROR
-import os
-from os import listdir
-from os.path import isfile, join
-import re
-
-import textwrap
 
 # List of characters in hex
 # buddy = 0x42 0x55 0x44 0x44 0x59
+
+
+
 term_character_list = [0x42, 0x55, 0x44, 0x44, 0x59]
+offset_values_list = []
+#character_test_range = [0x41, 0x5A]
+character_test_range = [0x1, 0x7F]
+all_searches_list = []
+
+output_file = './search_terms.txt'
+
+def find_smallest_number():
+    return min(term_character_list)
+
+def add_number_to_whole_list(number, list):
+    aux_list = []
+    for index in range(len(list)):
+        aux_list.append(list[index] + number)
+    return aux_list
 
 
-def find_smallest_number()
-{
-    print('aaa')
-}
+min_term = find_smallest_number()
+
+for index in range(len(term_character_list)):
+    offset_values_list.append(term_character_list[index] - min_term)
+
+#
+#
+
+
+for number in range(character_test_range[0], character_test_range[1]):
+    aux_list = add_number_to_whole_list(number, offset_values_list)
+    
+    all_searches_list.append(aux_list)
+
+
+search_type_hex = [0x73, 0x74, 0x72, 0x69, 0x6E, 0x67, 0x5C]
+newline_list = [0x0A]
+
+with open(output_file, 'wb') as OTPF:
+    for index in range(len(all_searches_list)):
+        see = all_searches_list[index]
+        #see2 = search_type_hex + see + newline_list
+        see2 = see + newline_list
+        OTPF.write( bytearray(see2) )
+
+
+print(term_character_list)
+print(min_term)
+print(character_test_range)
+print(offset_values_list)
+
+
